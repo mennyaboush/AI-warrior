14f92c9255bc3950f32dd588310a17e037a5b240
mennyaboush <mennyaboush@gmail.com>
2019-03-24 15:37:08 +0200

    pieceOfLand
    the maze gona get a class for him self.
    and he gona be build by a pieceOfLands

---
+++ PieceOfLand.cpp
@@ -0,0 +1,18 @@
+#include "PieceOfLand.h"
+
+
+
+PieceOfLand::PieceOfLand()
+{
+}
+
+
+PieceOfLand::~PieceOfLand()
+{
+}
+
+PieceOfLand::PieceOfLand(Point2D & location, int type, double saftyScore):location(&location),type(type),saftyScore(saftyScore)
+{
+}
+
+
